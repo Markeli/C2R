dca1963ee820f180acd01b1811070df91a1a2367
Maxim Markelow <max.markelow@gmail.com>
2018-04-25 01:14:43 +0300

    Decresed bot latency fot TelagramApi pushing

--- src/C2R.TelegramBot/Controllers/UpdateController.cs
+++ src/C2R.TelegramBot/Controllers/UpdateController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using C2R.TelegramBot.Services;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Telegram.Bot.Types;
 
 namespace C2R.TelegramBot.Controllers
@@ -13,17 +18,50 @@ namespace C2R.TelegramBot.Controllers
         [NotNull]
         private readonly IUpdateService _updateService;
 
-        public UpdateController([NotNull] IUpdateService updateService)
+        [NotNull]
+        private readonly BlockingCollection<Update> _updates;
+
+        [NotNull]
+        private readonly ILogger _logger;
+
+        private const int UpdatesCollectionMaxSize = 10000;
+        
+        public UpdateController([NotNull] IUpdateService updateService, 
+            [NotNull] ILogger<UpdateController> logger)
         {
             _updateService = updateService ?? throw new ArgumentNullException(nameof(updateService));
+            _logger = logger;
+            _updates = new BlockingCollection<Update>(UpdatesCollectionMaxSize);
+
+            Task.Factory.StartNew(async () => ProcessUpdatesAsync(), TaskCreationOptions.LongRunning);
+        }
+
+        private async Task ProcessUpdatesAsync()
+        {
+            foreach (var update in _updates.GetConsumingEnumerable())
+            {
+                try
+                {
+                    await _updateService
+                        .ProcessUpdateAsync(update)
+                        .ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"Error on update processig: {e.Message}", e);
+                }
+            }
         }
 
         // POST api/update
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]Update update)
+        public IActionResult Post([FromBody]Update update)
         {
-            await _updateService.ProcessUpdateAsync(update).ConfigureAwait(false);
-            return Ok();
+            if (update == null) return BadRequest();
+
+            return _updates.TryAdd(update)
+                ? Ok()
+                : StatusCode(500);
         }
     }
 }
